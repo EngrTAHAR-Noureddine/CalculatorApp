8a0cce2f37011db14c7f317ec9958ff0fb551be6
TAHAR <engr.tahar.noureddine@gmail.com>
2021-06-05 06:12:56 +0100

    add DarkTheme part1

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,40 @@ class MyApp extends StatelessWidget {
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        
+        primarySwatch: Colors.red,
+        primaryColor: Colors.black ,
+
+        backgroundColor: Colors.black ,
+
+        indicatorColor: Color(0xff0E1D36) ,
+        buttonColor: Color(0xff3B3B3B) ,
+
+        hintColor: Color(0xff280C0B) ,
+        highlightColor: Color(0xff372901) ,
+        hoverColor:  Color(0xff3A3A3B),
+
+        focusColor: Color(0xff0B2512),
+        disabledColor: Colors.grey,
+
+        cardColor:  Color(0xFF151515) ,
+        canvasColor: Colors.black ,
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(color: Colors.blue),
+        ),
+        bottomAppBarColor: Colors.black,
+        cardTheme: CardTheme(
+            color: Colors.black
+        ),
+        popupMenuTheme: PopupMenuThemeData(color: Colors.black),
+        bottomSheetTheme: BottomSheetThemeData(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: Colors.black,
+        tabBarTheme: TabBarTheme(labelColor: Colors.black,unselectedLabelColor: Colors.black),
+
+        buttonTheme: Theme.of(context).buttonTheme.copyWith(
+            colorScheme:  ColorScheme.dark() ),
+        appBarTheme: AppBarTheme(
+          elevation: 0.0,
+        ),
         /* dark theme settings */
       ),
       themeMode: ThemeMode.dark,
